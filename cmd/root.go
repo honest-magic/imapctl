c7f1d793ab9b24696dd0ed49237ce7398bb94740
Michael Szediwy <michael@szediwy.ch>
2023-04-15 22:58:52 +0200

    set version to 0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ var rootCmd = &cobra.Command{
 	Use:     "imapctl",
 	Short:   "Control and manage IMAP based mailboxes",
 	Long:    "Control and manage IMAP based mailboxes",
-	Version: "0.0.2",
+	Version: "0.0.3",
 	RunE:    imapCtrl,
 }
 
